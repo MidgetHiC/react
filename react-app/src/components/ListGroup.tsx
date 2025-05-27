f1e1561465e75409c451beca186a5fd7ee86244f
mbello7 <malik.bello@gtri.gatech.edu>
2025-05-27 10:25:55 -0400

    1hr into the React tutorial, 20 minutes till completion

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,37 @@
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+/*import { MouseEvent } from "react";*/
+import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void; // onClick
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  /*Hook specifically a state hook*/
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //items = [];
 
   return (
     <>
-      <h1>List</h1> {/*React.createElement('h1')*/}
+      <h1>{heading}</h1> {/*React.createElement('h1')*/}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">{item}</li>
+        {items.map((item, index) => (
+          <li 
+            className={ 
+              selectedIndex === index 
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            key={item} 
+            onClick={() => { 
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
